26deec0f514605f1207deedf1d8751c580f7e26c
midadhkappil <midadhkappil2@gmail.com>
2023-12-23 00:37:33 +0530

    followrequest

--- src/Components/Cards/index.js
+++ src/Components/Cards/index.js
@@ -1,18 +1,29 @@
 
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
+import { Button } from 'react-bootstrap';
+import { createFollowRequest } from 'Pages/Followers/api';
 const CustomCard = ({twit}) => {
+
+  const onClickFollow = async () => {
+    const response = await createFollowRequest({
+      recipient: twit.user._id
+      
+    })
+  }
+
   return (
     <Card className="mb-3">
       <Card.Body>
         <div className="d-flex align-items-center mb-3">
           <Image src={""} roundedCircle style={{ width: '40px', height: '40px' }} />
-          <span className="ms-2 fw-bold">Midad{""}</span>
+          <span className="ms-2 fw-bold">{twit?.user?.username}</span>
         </div>
         <Card.Text>{twit.destination}</Card.Text>
         <Card.Text>{twit.startDate}</Card.Text>
         <Card.Text>{twit.endDate}</Card.Text>
         <Card.Text>{twit.content}</Card.Text>
+        <Button onClick={onClickFollow}>Follow</Button>
        
       </Card.Body>
     </Card>
